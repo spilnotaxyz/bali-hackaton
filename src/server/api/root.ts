f32c60de8821cb019c29979183b89d0cd3a8b8cb
didostap <dribnucko@gmail.com>
2023-05-09 12:29:44 +0800

    proposal router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { partnershipRouter } from "~/server/api/routers/partnership/partnership.router";
+import { proposalRouter } from "~/server/api/routers/proposal/proposal.router";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { partnershipRouter } from "~/server/api/routers/partnership/partnership.
  */
 export const appRouter = createTRPCRouter({
   partnership: partnershipRouter,
+  proposal: proposalRouter,
 });
 
 // export type definition of API
