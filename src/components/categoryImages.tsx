d10b7a8f6e4c5097110b191e387db6938059c0c4
Dmitry Alekseev <pernifin@gmail.com>
2023-05-10 21:09:41 +0800

    Different colors for categories. Images for dashboard tiles

--- src/components/categoryImages.tsx
+++ src/components/categoryImages.tsx
@@ -2,22 +2,35 @@ import { Megaphone, Microscope, Search, User2, Landmark, Users, Boxes, Bitcoin }
 import { type Category } from "@prisma/client";
 import { cn } from "~/lib/utils";
 
-export default function CategoryImage({ category, className }: { category: Category, className?: string }) {
-  const images = {
-    MARKETING: Megaphone,
-    RESEARCH: Microscope,
-    AUDIT: Search,
-    ADVISOR: User2,
-    INVESTOR: Landmark,
-    COLLAB: Users,
-    PARTNERSHIPS: Boxes,
-    OTHER: Bitcoin,
-  };
+const images = {
+  MARKETING: Megaphone,
+  RESEARCH: Microscope,
+  AUDIT: Search,
+  ADVISOR: User2,
+  INVESTOR: Landmark,
+  COLLAB: Users,
+  PARTNERSHIPS: Boxes,
+  OTHER: Bitcoin,
+};
 
+const colors = {
+  MARKETING: 'from-amber-500 to-amber-600',
+  RESEARCH: 'from-lime-500 to-lime-600',
+  AUDIT: 'from-orange-500 to-orange-600',
+  ADVISOR: 'from-emerald-500 to-emerald-600',
+  INVESTOR: 'from-sky-500 to-sky-600',
+  COLLAB: 'from-yellow-500 to-yellow-600',
+  PARTNERSHIPS: 'from-indigo-500 to-indigo-600',
+  OTHER: 'from-fuchsia-500 to-fuchsia-600',
+}
+
+export default function CategoryImage({ category, className }: { category: Category, className?: string }) {
   const Element = images[category];
+  const gradient = colors[category];
   return (
     <div className={cn(
-      "w-full h-full flex items-center justify-center bg-gradient-to-r from-amber-500 to-orange-600",
+      "w-full h-full flex items-center justify-center bg-gradient-to-r ",
+      gradient,
       className
     )}>
       <Element size={96} className="text-slate-800" />
